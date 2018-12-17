c4f5158cad0e0e1e66361c4b8fe719fde3638219
Kaikai Zhu <kaikai4989@gmail.com>
2018-12-17 13:19:02 -0800

    Add a YteResult helper class. UnifiedTemplateResolver still needs to update the API to accept only one Uint8List.

--- lib/ui/yte.dart
+++ lib/ui/yte.dart
@@ -2,6 +2,16 @@
 
 part of dart.ui;
 
+class YteResult {
+  YteResult._(ByteData raw)
+      : errorCode = raw.getUint8(0) + (raw.getUint8(1) << 8),
+        content = raw.buffer.asUint8List(2);
+  final int errorCode;
+  final Uint8List content;
+  bool get ok => errorCode == 0;
+  String get errorMessage => ok ? null : '$errorCode: ' + String.fromCharCodes(content);
+}
+
 /// See https://groups.google.com/forum/#!topic/flutter-dev/H0mcfMOMcjY for why NativeFieldWrapperClass2 is required.
 class YteUnifiedResolver extends NativeFieldWrapperClass2 {
   YteUnifiedResolver._() {
@@ -10,12 +20,12 @@ class YteUnifiedResolver extends NativeFieldWrapperClass2 {
 
   static final YteUnifiedResolver _singleton = YteUnifiedResolver._();
 
-  static Uint8List resolve(Uint8List templateConfig, Uint8List model, Uint8List hostProperties) =>
-      _singleton._resolve(templateConfig, model, hostProperties);
+  static YteResult resolve(Uint8List templateConfig, Uint8List model, Uint8List hostProperties) =>
+      YteResult._(_singleton._resolve(templateConfig, model, hostProperties));
 
   // ======= Below are C++ natives =======
 
-  Uint8List _resolve(Uint8List templateConfig, Uint8List model, Uint8List hostProperties) native 'YteUnifiedResolver_resolve';
+  ByteData _resolve(Uint8List templateConfig, Uint8List model, Uint8List hostProperties) native 'YteUnifiedResolver_resolve';
 
   void _constructor() native 'YteUnifiedResolver_constructor';
 }
@@ -27,10 +37,11 @@ class YteEkoProcessor extends NativeFieldWrapperClass2 {
     _constructor(transform);
   }
 
+  YteResult process(Uint8List input) => YteResult._(_process(input));
+
   // ======= Below are C++ natives =======
 
-  Uint8List process(Uint8List input) native 'YteEkoProcessor_process';
+  ByteData _process(Uint8List input) native 'YteEkoProcessor_process';
 
   void _constructor(Uint8List transform) native 'YteEkoProcessor_constructor';
 }
-
